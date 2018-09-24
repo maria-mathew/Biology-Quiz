e871574080c02b4967416b750762080576ca2994
Maria Mathew <43054463+maria-mathew@users.noreply.github.com>
2018-09-24 13:57:25 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,11 @@
+/*javascript for index.html*/
+/*-----MARIA MATHEW-------
+------ID: 991491575--------*/
+
+//function to alert the instructions to the user onclick of 'help' buttton.
+function help(){
+	alert("The Game consists of 10 questions."+
+			" There is no time limit."+
+			" You must answer all the questions."+
+			" Press 'Submit my answers' once you complete.");
+}
